6e65c4c0fcbcc1ea7e9723cb270d6307109e9266
msb <mike@msb.me.uk>
2022-12-10 08:23:32 +0000

    generating all the JSON resource files basically working :D

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,61 @@ import React, { useState } from 'react'
 import './App.css'
 import * as JSZip from 'jszip'
 import { saveAs } from 'file-saver'
+import slugify from 'slugify'
+
+/*
+  TODO:
+  /assets/cinemashow/shows.json
+  /assets/cinemashow/show_{showSlug}.json
+  /assets/cinemashow/lang/en_us.json (?)
+  /assets/cinemashow/models/item/show_{showSlug}.json
+  /assets/cinemashow/models/block/screen_base.json (?)
+  /assets/cinemashow/models/block/show_{showSlug}_{x}_{y}.json
+  /assets/cinemashow/blockstates/show_{showSlug}.json
+ */
 
 const PIXELS_PER_BLOCK = 16
+
+/**
+ * Upper limit of blocksX.
+ */
+const BLOCKS_X_MAX = 10
+
+/**
+ * Upper limit of blocksY.
+ */
+const BLOCKS_Y_MAX = 10
+
+/**
+ * Default value if `franeTime` not set.
+ */
+const DEFAULT_FRAME_TIME = 25
+
 // FIXME
-const NUMBER_OF_FRAMES = 8
+const NUMBER_OF_FRAMES = 17
+
+const ROTATIONS: [string, {}][] = [
+  ["north", {}],
+  ["east", {y: 90}],
+  ["south", {y: 180}],
+  ["west", {y: 270}],
+  ["north_down", {x: 90}],
+  ["east_down", {x: 90, y: 90}],
+  ["south_down", {x: 90, y: 180}],
+  ["west_down", {x: 90, y: 270}],
+  ["north_up", {x: 270}],
+  ["east_up", {x: 270, y: 90}],
+  ["south_up", {x: 270, y: 180}],
+  ["west_up", {x: 270, y: 270}],
+]
 
 type Point = {
   x: number
   y: number
+}
+
+function toJson(value: any): string {
+  return JSON.stringify(value, null, 2)
 }
 
 function readFile(file: Blob) {
@@ -49,18 +96,17 @@ function canvasToBlob(canvas: HTMLCanvasElement) {
 function App() {
 
   const [min2ndAxisLength, setMin2ndAxisLength] = useState(Number.MAX_SAFE_INTEGER)
-  const [blocksX, setBlocksX] = useState(0)
-  const [blocksY, setBlocksY] = useState(4)
+  const [blocksX, setBlocksX] = useState(BLOCKS_X_MAX)
+  const [blocksY, setBlocksY] = useState(0)
   const [xDefinedAxis, setXDefinedAxis] = useState(false)
-  const [showName, setShowName] = useState("Clowning")
-  const [showSlug, setShowSlug] = useState("clowning")
-  const [frameTime, setFrameTime] = useState(8)
+  const [showName, setShowName] = useState("Good Vintage")
+  const [frameTime, setFrameTime] = useState(2)
 
   function drawImage(image: HTMLImageElement, canvas: HTMLCanvasElement, min2ndAxisLength: number): number {
     let width
     let height
   
-    if (xDefinedAxis) {
+    if (blocksY === 0) {
       width = blocksX * PIXELS_PER_BLOCK
       height = width * image.height / image.width
       min2ndAxisLength = Math.min(min2ndAxisLength, height)
@@ -75,14 +121,6 @@ function App() {
     context && context.drawImage(image, 0, 0, width, height)
   
     return min2ndAxisLength
-  }
-
-  function getOffset(imageWidth: number, imageHeight: number): Point {
-    if (xDefinedAxis) {
-      return {x: 0, y: (imageHeight - min2ndAxisLength) / 2}
-    } else {
-      return {x: (imageWidth - min2ndAxisLength) / 2, y: 0}
-    }
   }
 
   async function handleFileEvent(event: React.ChangeEvent<HTMLInputElement>) {
@@ -103,15 +141,25 @@ function App() {
     // rounds `min2ndAxisLength` down to the nearest `PIXELS_PER_BLOCK` (idempotent)
     min2ndAxisLength -= min2ndAxisLength % PIXELS_PER_BLOCK
 
-    if (xDefinedAxis) {
+    if (blocksY === 0) {
+      setXDefinedAxis(true)
       setBlocksY(min2ndAxisLength / PIXELS_PER_BLOCK)
     } else {
+      setXDefinedAxis(false)
       setBlocksX(min2ndAxisLength / PIXELS_PER_BLOCK)
     }
     setMin2ndAxisLength(min2ndAxisLength)
     console.log(min2ndAxisLength / PIXELS_PER_BLOCK)
-    }
+  }
 
+  function getOffset(imageWidth: number, imageHeight: number): Point {
+    if (xDefinedAxis) {
+      return {x: 0, y: (imageHeight - min2ndAxisLength) / 2}
+    } else {
+      return {x: (imageWidth - min2ndAxisLength) / 2, y: 0}
+    }
+  }
+  
   async function generateTextures() {
 
     const response = await fetch("/cinemashow-0.3.jar")
@@ -121,12 +169,56 @@ function App() {
     }
     const zip = await JSZip.loadAsync(response.blob())
 
+    // we can use a single canvas
+    const output = document.createElement('canvas')
+    output.width = PIXELS_PER_BLOCK
+    output.height = PIXELS_PER_BLOCK * NUMBER_OF_FRAMES
+
+    const assetsPath = "/assets/cinemashow"
+    const commonName = `show_${slugify(showName, {replacement: '_', lower: true, strict: true})}`
+
+    // FIXME shouldn't overwrite
+    zip.file(`${assetsPath}/shows.json`, toJson([commonName]))
+
+    // TODO I don't think we use `frameTime`
+    zip.file(`${assetsPath}/${commonName}.json`, toJson({showName, frameTime, blocksX, blocksY}))
+
+    const messageResource: { [key: string]: string } = {
+      "itemGroup.cinemashow": "Cinema Show"
+    }
+    messageResource[`block.cinemashow.${commonName}`] = showName
+    // FIXME shouldn't overwrite
+    zip.file(`${assetsPath}/lang/en_us.json`, toJson(messageResource))
+
+    const textureBack = {texture: "#back"}
+
+    zip.file(`${assetsPath}/models/item/${commonName}.json`, toJson({
+      parent: "minecraft:block/cube_all",
+      elements: [
+        {
+          faces: {
+            down: textureBack,
+            east: textureBack,
+            north: textureBack,
+            south: textureBack,
+            up: {texture: "#screen"},
+            west: textureBack
+          },
+          from: [0, 0, 0],
+          to: [16, 16, 16]
+        }
+      ],
+      textures: {
+        back: "cinemashow:block/screen_base",
+        screen: "cinemashow:block/screen_item"
+      }
+    }))
+
+    // FIXME don't use any
+    const variants: { [key: string]: {} } = {}
+
     for (let x = 0; x < blocksX; x ++) {
       for (let y = 0; y < blocksY; y ++) {
-        // FIXME
-        const output = document.getElementById(`${x}_${y}`) as HTMLCanvasElement
-        output.width = PIXELS_PER_BLOCK
-        output.height = PIXELS_PER_BLOCK * NUMBER_OF_FRAMES
         const outputContext = output.getContext('2d')
         if (outputContext) {
           for (let f = 0; f < NUMBER_OF_FRAMES; f ++) {
@@ -142,16 +234,48 @@ function App() {
             )
           }
         }
+        const tileResourceName = `${commonName}_${x}_${y}`
+        const textureName = `${assetsPath}/textures/block/${tileResourceName}.png`
         const blob = await canvasToBlob(output)
-        const textureName = `/assets/cinemashow/textures/block/show_${showSlug}_${x}_${y}.png`
         zip.file(textureName, blob, {base64: true})
-        zip.file(`${textureName}.mcmeta`, JSON.stringify({animation: {frametime: frameTime}}))
+        zip.file(`${textureName}.mcmeta`, toJson({animation: {frametime: frameTime}}))
+
+        zip.file(`${assetsPath}/models/block/${tileResourceName}.json`, toJson({
+          parent: "minecraft:block/cube_all",
+          elements: [
+            {
+              faces: {
+                down: textureBack,
+                east: textureBack,
+                north: {texture: "#screen"},
+                south: textureBack,
+                up: textureBack,
+                west: textureBack
+              },
+              from: [0, 0, 0],
+              to: [16, 16, 16]
+            }
+          ],
+          textures: {
+            back: "cinemashow:block/screen_base",
+            screen: `cinemashow:block/${tileResourceName}`
+          }
+        }))
+
+        ROTATIONS.forEach(([facing, rotation]) => {
+          variants[`facing=${facing},x=${x},y=${y}`] = {
+            ...rotation,
+            model: `cinemashow:block/${tileResourceName}`,
+          }
+        })
       }
     }
 
+    zip.file(`${assetsPath}/blockstates/${commonName}.json`, toJson({variants}))
+  
     const content = await zip.generateAsync({type:"blob"})
     saveAs(content, "cinemashow.done.jar")
-}
+  }
 
   return (
     <div className="App">
@@ -160,7 +284,7 @@ function App() {
       </div>
       <div>
         {
-          [0, 1, 2, 3, 4, 5, 6, 7].map(index => (
+          Array.from(Array(NUMBER_OF_FRAMES).keys()).map(index => (
             <span key={`f_${index}`}>
               <canvas id={`f_${index}`} width={10} height={10} />
               &nbsp;
@@ -169,19 +293,7 @@ function App() {
         }
       </div>
       <div>
-        <button onClick={generateTextures}>Generate Textures</button>
-      </div>
-      <div>
-        {
-          [0, 1, 2, 3].map(x => (
-            [0, 1, 2, 3].map(y => (
-              <span key={`${x}_${y}`}>
-                <canvas id={`${x}_${y}`} width={10} height={10} />
-                &nbsp;
-              </span>
-            ))
-          ))
-        }
+        <button onClick={generateTextures}>Generate Resources</button>
       </div>
     </div>
   )
